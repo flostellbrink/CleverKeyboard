46c28d67ef0957c67691c6bd952598f829726f78
Florian Stellbrink <flo@stellbr.ink>
2020-09-19 01:13:15 +0200

    Add tray icon

---
+++ CleverKeyboard/Commands/ExitCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace CleverKeyboard.Commands
+{
+	public class ExitCommand : MarkupExtension, ICommand
+	{
+		private static ExitCommand Instance { get; } = new ExitCommand();
+
+#pragma warning disable
+		public event EventHandler CanExecuteChanged;
+#pragma warning restore
+
+		public bool CanExecute(object _)
+		{
+			return true;
+		}
+
+		public void Execute(object _)
+		{
+			Application.Current.Shutdown();
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return Instance;
+		}
+	}
+}
---
+++ CleverKeyboard/Commands/OpenCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace CleverKeyboard.Commands
+{
+	public class OpenCommand : MarkupExtension, ICommand
+	{
+		private static OpenCommand Instance { get; } = new OpenCommand();
+
+#pragma warning disable
+		public event EventHandler CanExecuteChanged;
+#pragma warning restore
+
+		public bool CanExecute(object _)
+		{
+			return true;
+		}
+
+		public void Execute(object _)
+		{
+			var window = Application.Current.MainWindow;
+			if (window == null) return;
+
+			window.WindowState = WindowState.Normal;
+			Application.Current.MainWindow?.Focus();
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return Instance;
+		}
+	}
+}
